acc720e97410cfdfc91d293c8771cc951af0b61c
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-26 15:15:58 -0600

    Athletes
    Added required class items for athlete and athletes which is a list of athletes.

---
+++ HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/Athletes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingServer
+{
+    public class Athletes
+    {
+        List<Athlete> ListOfAthletes;
+        
+        public Athletes(List<Athlete> _ListOfAthletes)
+        {
+            ListOfAthletes = new List<Athlete>(_ListOfAthletes);
+        }
+    }
+}
