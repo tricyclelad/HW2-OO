72aa1d43ac995a6f206876de2346dcf8c6588dfc
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-25 19:44:03 -0600

    Added more code and communicator.
    Added the solution and finally figured out how to use the communicator. Need to figure how to get the messages and print them to the screen or send to other endpoints

---
+++ HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Base;
+
+namespace TrackingServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Communicator communicator = new Communicator(12000);
+            while (!communicator.IsMessageAvailable())
+                Console.WriteLine("Message Not Ready");
+
+            Console.WriteLine("Message Ready:");
+            Console.ReadKey();
+        }
+    }
+}
