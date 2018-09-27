e77b0fdaa03c8d01120ccf6cb3991c37206f1014
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-26 18:20:56 -0600

    FinishedUpdate
    Finished FinishedUPdate Processor and Unit Test

--- HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/MessageProcessors/FinishedUpdateProcessor.cs
+++ HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/MessageProcessors/FinishedUpdateProcessor.cs
@@ -10,7 +10,17 @@ namespace TrackingServer
     {
         public void Process(string[] Message, ref RaceManager _MyRaceManager)
         {
-
+            foreach (var athlete in _MyRaceManager.MyRunners)
+            {
+                if (athlete.bibNumber == Convert.ToInt32(Message[1]))
+                {
+                    athlete.lastUpdatedTime = Convert.ToDouble(Message[2]);
+                    athlete.finishTime = athlete.lastUpdatedTime - athlete.startTime; 
+                    athlete.status = "Finished!";
+                    //Communicator Logic here
+                    
+                }
+            }
         }
     }
 }
