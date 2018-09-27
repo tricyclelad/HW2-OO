f3bd6b5b4e8b86335f366bb6804afc4bd0b5d12f
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-26 21:54:32 -0600

    Finished Subscribe Processor and unit test

--- HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/MessageProcessors/SubscribeProcessor.cs
+++ HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/MessageProcessors/SubscribeProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackingServer;
 
 namespace TrackingServer
 {
@@ -10,6 +11,23 @@ namespace TrackingServer
     {
         public void Process(string [] Message, ref RaceManager _MyRaceManager)
         {
+            foreach (var client in _MyRaceManager.MyClients)
+            {
+                if (client.IPaddress == Message[2] && client.EndPoint == Message[3])
+                {
+                    foreach (var runner in _MyRaceManager.MyRunners)
+                    {
+                        if (Convert.ToInt32(Message[1]) == runner.bibNumber)
+                        {
+                            runner.Attach(client);
+                        }
+
+                    }
+
+                }
+            }
+            //Since I can't get the communicator working, we'll pretend that the message is
+            //in this order unsubscribe,bibnumber,ipaddress,endpoint
 
         }
     }
