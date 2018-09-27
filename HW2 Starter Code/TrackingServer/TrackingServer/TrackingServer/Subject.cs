6796892efcf1cf23d556bcf0fce7c248d62b0ba2
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-26 19:57:23 -0600

    Created observer and subject abstract classes

---
+++ HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/Subject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingServer
+{
+    abstract class Subject
+    {
+        private List<Observer> _observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+        public void Notify()
+        {
+            foreach (Observer ob in _observers)
+            {
+                ob.Update();
+            }
+        }
+    }
+}
