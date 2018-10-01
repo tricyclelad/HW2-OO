b5609279cd3e31689957be9a99c9fb29a0164ce7
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-30 21:24:03 -0600

    Updated subscribe and unsubscribe methods

--- HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/MessageProcessors/UnsubscribeProcessor.cs
+++ HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/MessageProcessors/UnsubscribeProcessor.cs
@@ -10,26 +10,26 @@ namespace TrackingServer
     {
         public void Process(string[] Message, ref RaceManager _MyRaceManager, System.Net.IPEndPoint senderEndPoint)
         {
-            //foreach (var client in _MyRaceManager.MyClients)
-            //{
-            //    if (client.IPaddress==Message[2] && client.EndPoint==Message[3])
-            //    {
-            //        foreach (var runner in _MyRaceManager.MyRunners)
-            //        {
-            //            if (Convert.ToInt32(Message[1]) == runner.bibNumber)
-            //            {
-            //                runner.Detach(client);
-            //            }
+            foreach (var client in _MyRaceManager.MyClients)
+            {
+                if (client.MyEndPoint.Address == senderEndPoint.Address && client.MyEndPoint.Port == senderEndPoint.Port)
+                {
+                    foreach (var runner in _MyRaceManager.MyRunners)
+                    {
+                        if (Convert.ToInt32(Message[1]) == runner.bibNumber)
+                        {
+                            runner.Detach(client);
+                        }
 
-            //        }        
+                    }
 
-            //    }
-            //}
+                }
+            }
 
             //Since I can't get the communicator working, we'll pretend that the message is
             //in this order unsubscribe,bibnumber,ipaddress,endpoint
-            
-            
+
+
         }
     }
 }
