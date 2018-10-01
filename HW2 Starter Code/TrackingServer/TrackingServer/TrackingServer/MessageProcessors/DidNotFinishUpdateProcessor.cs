46dcb8a9b6ed29393495cfb19d0584dd1d8b273b
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-30 21:01:55 -0600

    Updated the didnotfinish update processor

--- HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/MessageProcessors/DidNotFinishUpdateProcessor.cs
+++ HW2 Starter Code/TrackingServer/TrackingServer/TrackingServer/MessageProcessors/DidNotFinishUpdateProcessor.cs
@@ -15,8 +15,17 @@ namespace TrackingServer
                 if (athlete.bibNumber == Convert.ToInt32(Message[1]))
                 {
                     athlete.lastUpdatedTime = Convert.ToDouble(Message[2]);
-                    athlete.finishTime = -1;
-                    athlete.status = "DropOut!";
+                    athlete.finishTime = athlete.lastUpdatedTime;
+                    athlete.status = "did not finish";
+                    string outGoingMessage = "Status," + athlete.bibNumber + "," + athlete.status + "," + athlete.startTime + "," + athlete.distanceCovered + "," + athlete.lastUpdatedTime + "," + athlete.finishTime;
+                    foreach (var client in _MyRaceManager.MyClients)
+                    {
+                        if (client.MyAthleteSubject.bibNumber == athlete.bibNumber)
+                        {
+                            _MyRaceManager.MyCommunicator.Send(outGoingMessage, client.MyEndPoint);
+                        }
+
+                    }
                     //Communicator Logic here
                 }
             }
